f71139161cb3e1091a8da6002009c6240c9da2d3
Tess718 <111273602+Tess718@users.noreply.github.com>
2024-03-22 20:17:26 +0000

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -31,7 +31,7 @@ function home() {
              trigger: text,
              scrub: true,
              start: 'top 90%', 
-             end: 'bottom 60%',
+             end: 'bottom bottom',
              toggleActions: 'play none none reverse'
            }
          }
