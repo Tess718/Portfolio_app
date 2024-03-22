59cb2b702beae05db30d2968aca5e8e2923df986
Tess718 <111273602+Tess718@users.noreply.github.com>
2024-03-22 19:31:55 +0000

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -264,7 +264,7 @@ function home() {
                     <div className="row justify-content-between">
                       <div className="col-lg-6 my-auto">
                         <h3 className="pb-4"><span className="title">services</span></h3>
-                        <p className="reveal-type">I specialize in comprehensive web development services, including crafting visually appealing and responsive user interfaces, building dynamic single-page applications, implementing web designs seamlessly, ensuring responsiveness across devices, collaborating effectively on projects, optimizing code for enhanced performance, providing insights into the latest tech trends, and offering custom solutions tailored to unique project needs. Let's collaborate to bring innovation and efficiency to your digital projects and create a standout online presence.</p>
+                        <p className="reveal-type pb-5 pb-lg-0">I specialize in comprehensive web development services, including crafting visually appealing and responsive user interfaces, building dynamic single-page applications, implementing web designs seamlessly, ensuring responsiveness across devices, collaborating effectively on projects, optimizing code for enhanced performance, providing insights into the latest tech trends, and offering custom solutions tailored to unique project needs. Let's collaborate to bring innovation and efficiency to your digital projects and create a standout online presence.</p>
                       </div>
                       <div className="col-lg-5">
                         <div className="segrid">
